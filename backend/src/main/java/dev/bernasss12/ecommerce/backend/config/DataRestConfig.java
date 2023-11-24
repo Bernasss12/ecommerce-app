9476c0a60206c3bbc820f9c3b88a92c71bdda467
Bernardo Antunes <bernasss12@gmail.com>
2023-11-24 15:09:23 +0000

    Expose ids for entities

--- backend/src/main/java/dev/bernasss12/ecommerce/backend/config/DataRestConfig.java
+++ backend/src/main/java/dev/bernasss12/ecommerce/backend/config/DataRestConfig.java
@@ -1,6 +1,12 @@
 package dev.bernasss12.ecommerce.backend.config;
 
+import java.util.stream.Collectors;
+
 import dev.bernasss12.ecommerce.backend.entity.Product;
+import dev.bernasss12.ecommerce.backend.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,19 +16,35 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+    private EntityManager entityManager;
 
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions))
-                .withCollectionExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions));
-
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions))
-                .withCollectionExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions));
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((unused, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.exposeIdsFor(
+                entityManager
+                        .getMetamodel()
+                        .getEntities()
+                        .stream()
+                        .map(EntityType::getJavaType)
+                        .toList()
+                        .toArray(new Class[0])
+        );
+    }
 }
